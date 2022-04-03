78f9212153cf114a6ea8e49c7f518784853d11b3
iamjoseph1998 <iamjoseph1998@gmail.com>
2022-04-03 11:31:05 +0530

    Created location model and migrated to db

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+    coordinates = models.PointField(srid=4326)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Location'
